56c36465d845257833382343450e994af693695b
ap1 <anjul.patney@gmail.com>
2015-08-22 00:02:32 -0700

    first move to jekyll

--- past/2015/papers.php
+++
@@ -1,8 +0,0 @@
-<?php $pagetitle="Papers"; include("header.php") ?>
-
-<p>Below is a list of papers accepted to ACM I3D 2015.</p>
-
-<?php include("papers.html") ?>
-
-<?php include("footer.php") ?>
-
