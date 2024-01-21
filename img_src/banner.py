0277fcbcb575403db2b5bf175f875316e3216f61
Roc <rcrmn16@gmail.com>
2024-01-21 14:16:18 +0100

    Update logos and banners

--- img_src/banner.py
+++ img_src/banner.py
@@ -1,21 +1,43 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Create the banner image for the given year")
-parser.add_argument("year", type=int, help="What year is the banner for")
+parser.add_argument("year", type=int, help="For what year the banner should be generated. 0 to build the generic banners and logos.")
 args = parser.parse_args()
 
 import subprocess
 
-def svg2png(src, dst, year):
+def svg2png(src, dst, *, bg=False):
+    with open(src) as b:
+        svg = b.read()
+
+    args = ["inkscape", "--pipe", "-o", f"../img/{dst}"]
+    if bg:
+        args += ["-b", "#ffffff", "-y", "1.0"]
+    subprocess.run(args, input=svg, text=True)
+
+def svg2png_year(src, dst, year, *, bg=False):
     with open(src) as b:
         svg = b.read()
 
     svg = svg.replace("{{year}}", str(year))
 
     args = ["inkscape", "--pipe", "-o", f"../{year}/img/{dst}"]
+    if bg:
+        args += ["-b", "#ffffff", "-y", "1.0"]
     subprocess.run(args, input=svg, text=True)
 
 
-svg2png("banner_plain_template.svg", "banner.png", args.year)
-svg2png("banner_subtitle_template.svg", "banner-subtitle.png", args.year)
-svg2png("banner_narrow_subtitle_template.svg", "banner-narrow-subtitle.png", args.year)
+if args.year == 0:
+    svg2png("banner-general.svg", "banner.png")
+    svg2png("banner-general.svg", "banner-bg.png", bg=True)
+    svg2png("banner-youtube.svg", "banner-youtube.png")
+    svg2png("logo-square.svg", "logo-square.png")
+    svg2png("logo-square.svg", "logo-square-bg.png", bg=True)
+    svg2png("logo-square-title.svg", "logo-square-title.png")
+    svg2png("logo-square-title.svg", "logo-square-title-bg.png", bg=True)
+else:
+    svg2png_year("banner-year.svg", "banner.png", args.year)
+    svg2png_year("banner-year.svg", "banner-bg.png", args.year, bg=True)
+    svg2png_year("social-card-year.svg", "social-card.png", args.year)
+    svg2png_year("logo-square-title-year.svg", "logo-square-title.png", args.year)
+    svg2png_year("logo-square-title-year.svg", "logo-square-title-bg.png", args.year, bg=True)
---
+++ img_src/logo-ico.py
@@ -0,0 +1,16 @@
+import subprocess
+from pathlib import Path
+import shutil
+
+tmp = Path("./tmp")
+
+tmp.mkdir(exist_ok=True, parents=True)
+
+sizes = [16, 24, 32, 64, 128]
+
+for sz in sizes:
+	subprocess.run(["inkscape", "logo.svg", "-o", (tmp/f"{sz}.png").as_posix(), "-w", f"{sz}", "-h", f"{sz}"])
+
+subprocess.run(["magick", "convert", *[(tmp/f"{sz}.png").as_posix() for sz in sizes], "favicon.ico"])
+
+shutil.rmtree(tmp)
